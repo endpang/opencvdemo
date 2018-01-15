ca64fa8d5e24cb899646397294dcd9f73741b80c
endler <zhiweip@oupeng.com>
2018-01-15 14:15:39 +0800

    Initial Commit

---
+++ opencv/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  opencv
+//
+//  Created by zhiwei pang on 2018/1/15.
+//  Copyright © 2018年 zhiwei pang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
